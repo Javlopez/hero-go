e8e18f73c7a45b83e3a7821f9fca3dbf7fbb00ca
Javier Lopez Lopez <sjavierlopez@gmail.com>
2020-02-15 15:25:13 -0600

    Debug goroutines

--- goroutines/main.go
+++ goroutines/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
+
+	fmt.Printf("Start Number of CPUS: %d\n", runtime.NumCPU())
+	fmt.Printf("Start Number of Go routines: %d\n", runtime.NumGoroutine())
+
+	fmt.Println()
 
 	wg.Add(2)
 	go func() {
@@ -18,7 +24,13 @@ func main() {
 		fmt.Println("Hello form two")
 		wg.Done()
 	}()
-
+	fmt.Printf("During the process | Number of CPUS: %d\n", runtime.NumCPU())
+	fmt.Printf("During the process | Number of Go routines: %d\n", runtime.NumGoroutine())
+	fmt.Println()
 	wg.Wait()
 	fmt.Println("This is the exit")
+
+	fmt.Printf("End Number of CPUS: %d\n", runtime.NumCPU())
+	fmt.Printf("End Number of Go routines: %d\n", runtime.NumGoroutine())
+	fmt.Println()
 }
